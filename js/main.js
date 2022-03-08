7a3d44f15777b8f712f18704f115c0d813047050
Higor Chagas <higornchagas@gmail.com>
2022-03-08 16:46:39 -0300

    files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let minhaImagem = document.querySelector('img');
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
 
 minhaImagem.onclick = function() {
     let meuSrc = minhaImagem.getAttribute('src');
@@ -7,4 +9,25 @@ minhaImagem.onclick = function() {
     } else {
         minhaImagem.setAttribute('src', 'img/momonga.png');
     }
+}
+
+function defineNomeUsuario() {
+    let meuNome = prompt('Por favor, digite seu nome.');
+    if(!meuNome || meuNome === null) {
+        defineNomeUsuario();
+    } else {
+        localStorage.setItem('nome', meuNome);
+        meuCabecalho.textContent = `Bem vindo ao meu domínio, ${meuNome}`;
+    }
+
+    if(!localStorage.getItem('nome')) {
+        defineNomeUsuario();
+    } else {
+        let nomeGuardado = localStorage.getItem('nome');
+        meuCabecalho.textContent = `Bem vindo ao meu domínio, ${nomeGuardado}`;
+    }
+}
+
+meuBotao.onclick = () => {
+    defineNomeUsuario();
 }
